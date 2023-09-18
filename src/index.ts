e43de104dc0a5fb6574c55350723ed7f156ee4a7
Connor Peet <connor@peet.io>
2023-09-18 16:47:59 -0700

    add publish pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import { join } from "path";
-import { Worker } from "worker_threads";
+import { join } from 'path';
+import { Worker } from 'worker_threads';
 import {
   AsyncHostInterface,
   Channel,
   WorkerInterface,
   WorkerRPC,
-} from "../chrome-cxx/mnt/extension/WorkerRPC";
+} from '../chrome-cxx/mnt/extension/WorkerRPC';
+
+export type * from '../chrome-cxx/mnt/extension/WorkerRPC';
+
+/** Utility type to get the worker's return value from a method name. */
+export type MethodReturn<T extends keyof WorkerInterface> = ReturnType<
+  WorkerInterface[T]
+> extends Promise<infer R>
+  ? R
+  : ReturnType<WorkerInterface[T]>;
 
 export interface IWasmWorker {
   rpc: WorkerRPC<AsyncHostInterface, WorkerInterface>;
@@ -13,20 +22,15 @@ export interface IWasmWorker {
 }
 
 export function spawn(hostInterface: AsyncHostInterface): IWasmWorker {
-  const worker = new Worker(join(__dirname, "worker.js"));
-  worker.on("message", (data) =>
-    channel.onmessage?.(new MessageEvent("message", { data }))
-  );
+  const worker = new Worker(join(__dirname, 'worker.js'));
+  worker.on('message', (data) => channel.onmessage?.(new MessageEvent('message', { data })));
 
   const channel: Channel<AsyncHostInterface, WorkerInterface> = {
     onmessage: null,
     postMessage: (m) => worker.postMessage(m),
   };
 
-  const rpc = new WorkerRPC<AsyncHostInterface, WorkerInterface>(
-    channel,
-    hostInterface
-  );
+  const rpc = new WorkerRPC<AsyncHostInterface, WorkerInterface>(channel, hostInterface);
 
   return {
     rpc,
