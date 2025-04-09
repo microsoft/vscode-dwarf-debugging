396fede032a4bce907660fc8c7c4020ce56d04d4
Mikael Waltersson <mikael.waltersson@gmail.com>
2025-04-10 09:06:41 +1000

    Update reference to LICENSE file of imported code

--- wasm/symbols-backend/lib/WasmVendorPlugins.h
+++ wasm/symbols-backend/lib/WasmVendorPlugins.h
@@ -1,6 +1,6 @@
 // Copyright 2023 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the wasm/symbols-backend/LICENSE file.
 
 #ifndef EXTENSIONS_CXX_DEBUGGING_WASMVENDORPLUGINS_H_
 #define EXTENSIONS_CXX_DEBUGGING_WASMVENDORPLUGINS_H_
